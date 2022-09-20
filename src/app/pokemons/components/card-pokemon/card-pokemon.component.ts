c07b6dc535fa2768abb9bdd7990a135563b638b9
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-19 20:15:28 -0500

    feat(localstorage): favorites added

--- src/app/pokemons/components/card-pokemon/card-pokemon.component.ts
+++ src/app/pokemons/components/card-pokemon/card-pokemon.component.ts
@@ -1,14 +1,23 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-card-pokemon',
     templateUrl: './card-pokemon.component.html'
 })
-export class CardPokemonComponent {
+export class CardPokemonComponent implements OnInit {
     @Input() pokemons: any[] = [];
+    favorites: any[] = [];
 
     constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        const favoritesStorage: any = localStorage.getItem('favorites');
+        
+        if (favoritesStorage) {
+            this.favorites = JSON.parse(favoritesStorage);
+        }
+    }
 
     navigateTo(pokemon: any) {
         this.router.navigate(['/details',
@@ -21,5 +30,20 @@ export class CardPokemonComponent {
                 image: pokemon.sprites.other['official-artwork'].front_default
             }
         ]);
+    }
+
+    validateFavorites(pokemon: any): boolean {
+        return this.favorites.some((item: any) => item.id === pokemon.id);
+    }
+
+    addRemoveFavorites(pokemon: any) {
+        if (!this.validateFavorites(pokemon)) {
+            this.favorites = [...this.favorites, pokemon];
+        }
+        else {
+            this.favorites = this.favorites.filter((item: any) => item.id !== pokemon.id);
+        }
+        
+        localStorage.setItem('favorites', JSON.stringify(this.favorites));
     }
 }
