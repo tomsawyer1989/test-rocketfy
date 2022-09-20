22bbdb7cb31c6503db9c670a8f34a39e92e04c0e
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-09-19 21:50:43 -0500

    feat(FavoritesPage): favorites page added

--- src/app/pokemons/pages/favorites/favorites-page.component.ts
+++ src/app/pokemons/pages/favorites/favorites-page.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-favorites-page',
     templateUrl: './favorites-page.component.html'
 })
-export class FavoritesPageComponent {}
+export class FavoritesPageComponent implements OnInit {
+    favorites: any[] = [];
+
+    ngOnInit(): void {
+        const favoritesStorage: any = localStorage.getItem('favorites');
+        
+        if (favoritesStorage) {
+            this.favorites = JSON.parse(favoritesStorage);
+        }
+    }
+}
